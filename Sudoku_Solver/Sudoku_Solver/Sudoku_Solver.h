f619de0887cafafa901926389d229d52161c42a7
zhvkgj <zhvkgj@yandex.ru>
2017-11-06 18:44:35 +0300

    add print to the file

--- Sudoku_Solver/Sudoku_Solver/Sudoku_Solver.h
+++ Sudoku_Solver/Sudoku_Solver/Sudoku_Solver.h
@@ -2,6 +2,7 @@
 #define __SUDOKU_SOLVER_H__
 #include <string>
 #include <fstream>
+#include <iostream>
 
 class Sudoku
 {
@@ -10,6 +11,8 @@ private:
 	bool isSolved = false;
 
 public:
+	// field game constructor 
+	// reads Sudoku from file "test_args.txt"
 	Sudoku()
 	{
 		std::ifstream fin("test_args.txt");
@@ -19,12 +22,37 @@ public:
 			std::getline(fin, tempStr);
 			for (int j = 0; j < 9; j++)
 			{
-				table[i][j] = (int)(tempStr[j] - '0');
+				if (tempStr[j] == ' ')
+				{
+					table[i][j] = (int)0;
+				}
+				else table[i][j] = (int)(tempStr[j] - '0');
 			}
 			tempStr.clear();
 		}
 		fin.close();
+
 	}
+
+	//writes the solution to the file "test_result.txt"
+	void printSolution()
+	{
+		std::ofstream fout("test_result.txt", std::ios::trunc);
+		std::string tempStr;
+		for (int i = 0; i < 9; i++) 
+		{
+			for (int j = 0; j < 9; j++) 
+			{
+				if (table[i][j] == ' ') tempStr.push_back(' ');
+				else tempStr.push_back(table[i][j] + '0');
+			}
+			fout << tempStr <<'\n';
+			tempStr.clear();
+		}
+		fout.close();
+	}
+
+
 
 };
 
